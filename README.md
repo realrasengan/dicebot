fb8016d193c622bf59441cc14454209120ba5f34
rasengan <realrasengan@gmail.com>
2021-06-12 07:26:58 +0000

    Roll

---
+++ README.md
@@ -0,0 +1,20 @@
+# Little roll game to see who gets the highest score
+### v0.01 ALPHA
+
+## Installation Instructions
+```
+npm install irc
+node bot.js
+```
+
+# Code is alpha and may be refactored at anytime!
+
+## Setup Instructions
+You can edit the settings in lib/constants.js
+
+## Copyright and License
+Copyright (c) 2021 freenode limited
+All Rights Reserved
+
+Licensed under the MIT License
+
---
+++ bot.js
@@ -0,0 +1,69 @@
+const constants = require('./lib/constants.js');  // constants and settings
+const IRC = require('./lib/irc.js'); // connected IRC Client
+
+// Hack
+// TODO: Better way to ensure one instance of bot only (pid file method breaks if process exits unexpectedly)
+var app = require('express')();
+app.listen(22537);
+
+// Global Variables
+let isRolling=false;
+let participants=[];
+
+// Main listener
+IRC.addListener('raw',(message) => {
+  if(message.command==='PRIVMSG' &&
+    (message.args[0].toLowerCase()===constants.IRC_CHAN.toLowerCase()))
+      parse(message.nick,message.args[1]);
+});
+
+// Main bot processor
+function parse(from,msg) {
+  switch(msg.toLowerCase()) {
+    case "!roulette":  roulette(from);
+      break;
+    case "!roll":  roll(from);
+      break;
+    default:
+      break;
+  }
+}
+
+// roulette
+function roulette(from) {
+  if(!isRolling) {
+    IRC.say(constants.IRC_CHAN,"Starting roll, type !roll - you have 30 seconds");
+    isRolling=true;
+    setTimeout(() => {
+      isRolling=false;
+      IRC.say(constants.IRC_CHAN,"Roulette ended.");
+      highest=0;
+      highest_person='';
+      for ( var person in participants ) {
+        if(participants[person]>highest) {
+          highest=participants[person];
+          highest_person=person;
+        }
+      }
+      IRC.say(constants.IRC_CHAN,"The winner is " + highest_person + " with a score of " + highest);
+      participants=[];
+    },30000);
+  }
+  else {
+    IRC.say(constants.IRC_CHAN,"In a roulette right now.");
+  }
+}
+function roll(from) {
+  if(isRolling) {
+    if(typeof participants[from] === 'undefined') {
+      roll = (Math.floor(Math.random() * 100)+1);
+      IRC.say(constants.IRC_CHAN,constants.BOLD+from+': '+roll+' out of 100');
+      participants[from]=roll;
+    }
+    else
+      IRC.say(constants.IRC_CHAN,'You already rolled, '+from);
+  }
+  else {
+    IRC.say(constants.IRC_CHAN,"We're not rolling.");
+  }
+}
